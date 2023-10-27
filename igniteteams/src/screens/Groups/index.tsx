5340cf643161a1c1191a74ab4161196394c64299
Rafael Lopes Nogueira <nogueirarln@gmail.com>
2023-10-27 15:13:08 -0300

    header

--- igniteteams/src/screens/Groups/index.tsx
+++ igniteteams/src/screens/Groups/index.tsx
@@ -1,9 +1,10 @@
+import { Header } from "@components/Header";
 import * as s from "./styles";
 
 export function Groups() {
   return (
     <s.Container >
-       
+       <Header />
     </s.Container>
   );
 }
