78ea56fa60f5d5745d077cc6f438be395c1fb3a9
Rafael Lopes Nogueira <nogueirarln@gmail.com>
2023-10-27 15:23:11 -0300

    highlight component

---
+++ igniteteams/src/components/Highlight/index.tsx
@@ -0,0 +1,22 @@
+import * as s from './styles'
+
+type Props = {
+    title: string;
+    subtitle: string
+}
+
+export function Highlight({title, subtitle}: Props) {
+    return (
+        <s.Container>
+            <s.Title>
+                {title}
+            </s.Title>
+
+
+            <s.Subtitle>
+                {subtitle}
+            </s.Subtitle>
+        </s.Container>
+        
+    );
+}
